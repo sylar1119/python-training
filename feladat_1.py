41a0d7a12e481ff885851345fe9ba1948369db84
Kaló Tamás <sylar@gmail.hu>
2022-09-07 11:58:39 +0200

    Harmadik nap def

---
+++ feladat_1.py
@@ -0,0 +1,6 @@
+numbers = [10, 0, 15, 0, 32, -12, 54, -20, 80]
+sum = 0
+for n in numbers:
+    if n > 0: 
+        sum += n 
+print(sum)
---
+++ selection_advanced.py
@@ -0,0 +1,16 @@
+# írd ki egy szám abszolút értékét
+
+n = int(input("szám?"))
+if n > 0:
+    print(n)
+elif n < 0:
+    print(-n)
+
+# válaszd ki a kisebb számot
+i = 10
+j = 6
+
+if i > j:
+    print(j)
+elif j > i:
+    print(i)
